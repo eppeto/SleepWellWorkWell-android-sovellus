4a4a7e38973c38e3ea78d1995cdff6291b938592
eppeto <eppu.savolainen@edu.savonia.fi>
2019-10-27 12:36:01 +0200

    Lisätty liikunta.xml sisältö ja laitettu toiminnallisuutta liikunta.java tiedostoon

--- app/src/main/java/com/example/ravitsemussovellus/Liikunta.java
+++ app/src/main/java/com/example/ravitsemussovellus/Liikunta.java
@@ -1,13 +1,37 @@
 package com.example.ravitsemussovellus;
 
 import android.os.Bundle;
+import android.widget.NumberPicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Liikunta extends AppCompatActivity {
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.liikunta);
+
+
+        // Numberpicker kahdella tavalla tehty ja kumpikaan ei VIELÄ toimi.
+
+        // tuntien valinta
+        final NumberPicker hp = findViewById(R.id.hourpicker);
+        String[] hours = new String[10];
+        for(int i=0; i<hours.length; i++)
+            hours[i] = Integer.toString(i);
+        hp.setMaxValue(10);
+        hp.setMinValue(0);
+        hp.setDisplayedValues(hours);
+        hp.setValue(1);
+        hp.setWrapSelectorWheel(true);
+
+        // minuuttien valinta
+        NumberPicker mp = findViewById(R.id.minutepicker);
+        mp.setMaxValue(59);
+        mp.setMinValue(0);
+        mp.setWrapSelectorWheel(true);
+
     }
 }
