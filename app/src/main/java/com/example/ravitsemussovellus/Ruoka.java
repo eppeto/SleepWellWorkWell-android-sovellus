3b72d4afd0613894555a5aaf0de08264e9f3f1da
pennasonja <sonja.pennanen@edu.savonia.fi>
2019-11-08 11:35:12 +0200

    Datepicker dialogipohja

--- app/src/main/java/com/example/ravitsemussovellus/Ruoka.java
+++ app/src/main/java/com/example/ravitsemussovellus/Ruoka.java
@@ -1,5 +1,6 @@
 package com.example.ravitsemussovellus;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -7,6 +8,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
@@ -16,22 +18,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class Ruoka extends AppCompatActivity {
-    TextView tvCounter, tvTime;
+    TextView tvCounter, eText;
     Button btnIncreament;
     Button btnDecreament;
-    Button btnSave, btnBack;
+    Button btnSave, btnBack, btnVaihda;
     FloatingActionButton infoButton;
     int counter = 0;
     TimePicker timepicker;
     private Calendar calendar;
+    int day, month, year;
 
-    private ArrayList<String> Ruokatiedot;
+    //private ArrayList<String> Ruokatiedot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,12 +44,14 @@ public class Ruoka extends AppCompatActivity {
         setContentView(R.layout.ruoka);
         this.setFinishOnTouchOutside(false);
 
-        this.Ruokatiedot = new ArrayList<String>();
+        //this.Ruokatiedot = new ArrayList<String>();
 
         tvCounter = findViewById(R.id.tvCounter);
         btnIncreament = findViewById(R.id.btnIncreament);
         btnDecreament = findViewById(R.id.btnDecreament);
-        timepicker = (TimePicker) findViewById(R.id.timePicker);
+        eText=findViewById(R.id.eTextDate);
+        btnVaihda=findViewById(R.id.btnVaihda);
+        timepicker = findViewById(R.id.timePicker);
         btnSave = findViewById(R.id.btnSave);
         btnBack=findViewById(R.id.btnback);
         infoButton= findViewById(R.id.floatingActionButton);
@@ -55,15 +62,15 @@ public class Ruoka extends AppCompatActivity {
 
                 int hour = timepicker.getCurrentHour();
                 int minute = timepicker.getCurrentMinute();
-                Ruokatiedot.add(hour + ":" + minute);
+                //Ruokatiedot.add(hour + ":" + minute);
 
                 String count = tvCounter.getText().toString();
-                Ruokatiedot.add(count);
+                //Ruokatiedot.add(count);
 
-                String list = "";
+                /*String list = "";
                 for(String item : Ruokatiedot) {
                     list += item + "\n";
-                }
+                }*/
              
                 String text = "Tiedot Tallennettu";
                 //kokeiltu tulostaa tiedot mitkä tallennettu arraylistiin
@@ -79,9 +86,7 @@ public class Ruoka extends AppCompatActivity {
                 String text = "Tietoja ei ole tallennettu";
                 Toast.makeText(Ruoka.this, text, Toast.LENGTH_SHORT).show();
                 finish();
-
             }
-
         });
 
         //Kasvikset/marjat/hedelmät counter
@@ -106,6 +111,35 @@ public class Ruoka extends AppCompatActivity {
                 }
             }
         });
+        //datepicker
+        SimpleDateFormat currentDate = new SimpleDateFormat("dd/MM/yyyy");
+        Date todayDate = new Date();
+        String thisDate = currentDate.format(todayDate);
+        eText.setText(thisDate);
+
+        btnVaihda.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                DatePickerDialog.OnDateSetListener dpd = new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                          int dayOfMonth) {
+
+                        int s = monthOfYear + 1;
+                        String a = dayOfMonth + "/" + s + "/" + year;
+                        eText.setText(a);
+                    }
+                };
+
+
+                DatePickerDialog d = new DatePickerDialog(Ruoka.this, dpd, year, month, day);
+                d.show();
+
+            }
+        });
+
 
 
         //timepicker
