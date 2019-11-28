7925934594f75cdb37cd2e8ac405d42e3cc2abf8
eppeto <eppu.savolainen@edu.savonia.fi>
2019-11-28 15:05:06 +0200

    Uudet hymiöt Päivä-javaan laitettu osa tietojen viennistä textvieweihin. Hymiöiden vaihtaminen datan mukaan.

--- app/src/main/java/com/example/ravitsemussovellus/Paiva.java
+++ app/src/main/java/com/example/ravitsemussovellus/Paiva.java
@@ -1,9 +1,12 @@
 package com.example.ravitsemussovellus;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
@@ -31,6 +34,94 @@ public class Paiva extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_paiva, container, false);
+        View view = inflater.inflate(R.layout.fragment_paiva, container, false);
+        // Unen ja stressinmäärä kuvat
+        ImageView unenlaatu = (ImageView) view.findViewById(R.id.unikuva);
+        ImageView stressimaara = (ImageView) view.findViewById(R.id.stressikuva);
+
+        // Ruokailumäärä, marjakouralliset ja ruokailuväli kuvat ja lukumäärät
+        TextView ruokamaara = (TextView) view.findViewById(R.id.ruokamaaraLuku);
+        ImageView ruokamaarakuva = (ImageView) view.findViewById(R.id.ruokamaarakuva);
+        TextView marjamaara = (TextView) view.findViewById(R.id.marjamaaraLuku);
+        ImageView marjamaarakuva = (ImageView) view.findViewById(R.id.marjakuva);
+        TextView ruokavaliluku = (TextView) view.findViewById(R.id.ruokailuvaliLuku);
+        ImageView ruokavalikuva = (ImageView) view.findViewById(R.id.ruokailuvaliKuva);
+
+        // Liikunnan kuvat ja tekstit
+        TextView liikunnanyhteiskestoluku = (TextView) view.findViewById(R.id.liikuntamaaraLuku);
+        ImageView liikunnanyhteiskestokuva = (ImageView) view.findViewById(R.id.liikuntamaaraKuva);
+        TextView kestavyysluku = (TextView) view.findViewById(R.id.kestavyysLuku);
+        TextView liikkuvuusluku = (TextView) view.findViewById(R.id.liikkuvuusLuku);
+        TextView lihasvoimaluku = (TextView) view.findViewById(R.id.lihasvoimaLuku);
+
+
+
+        // Unen ja stressinmäärän kuvien muuttaminen
+        // unenlaatu erinomainen = 2131296262
+        // unenlaatu hyvä = 2131296266
+        // unenlaatu huono = 2131296264
+
+        // stressinmäärä ei ollenkaan = 2131296261
+        // stressinmäärä sopiva = 2131296265
+        // stressinmäärä liikaa = 2131296263
+
+
+            // Unen laadun kuvan muuttaminen
+        if (unilaatu == 2131296262){
+            unenlaatu.setImageResource(R.drawable.good);
+        }
+        if (unilaatu == 2131296266 ){
+            unenlaatu.setImageResource(R.drawable.neutral);
+        }
+        if (unilaatu == 2131296264 ){
+            unenlaatu.setImageResource(R.drawable.bad);
+        }
+
+            // Stressinmäärän kuvan muuttaminen
+        if (stressi == 2131296261 ){
+            stressimaara.setImageResource(R.drawable.good);
+        }
+        if (stressi == 2131296265 ){
+            stressimaara.setImageResource(R.drawable.neutral);
+        }
+        if (stressi == 2131296263 ){
+            stressimaara.setImageResource(R.drawable.bad);
+        }
+
+        // Ruokalun lukujen ja kuvan muuttaminen
+
+        //marjojen määrä
+        marjamaara.setText(String.valueOf(maara_ruoka));
+        if (maara_ruoka > 4){
+            marjamaarakuva.setImageResource(R.drawable.good);
+        }
+        if (maara_ruoka < 5 && maara_ruoka > 1 ){
+            marjamaarakuva.setImageResource(R.drawable.neutral);
+        }
+        if (maara_ruoka < 2){
+            marjamaarakuva.setImageResource(R.drawable.bad);
+        }
+        // TÄHÄN MARJOJEN YHTEISMÄÄRÄN LASKEMINEN JA SIJOITUS
+
+
+        // Liikunta
+
+        if (tyyppi.equals("kestävyys")){
+            String[] parts = kesto.split(":");
+            kestavyysluku.setText(parts[0]+"h "+parts[1]+"min");
+        }
+        if (tyyppi.equals("liikkuvuus")){
+            String[] parts = kesto.split(":");
+            liikkuvuusluku.setText(parts[0]+"h "+parts[1]+"min");
+        }
+        if (tyyppi.equals("lihasvoima")){
+            String[] parts = kesto.split(":");
+            lihasvoimaluku.setText(parts[0]+"h "+parts[1]+"min");
+        }
+
+        Log.d("TESTI",tyyppi);
+        Log.d("KESTO",kesto);
+        return view;
     }
+
 }
